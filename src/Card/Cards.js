600371bd9b39f90ad78850ee9757ed5cc55dc795
sumanbiswas18 <sumanbiswas0188@gmail.com>
2019-06-28 16:27:49 +0530

    adding css module card components.

--- src/Card/Cards.js
+++ src/Card/Cards.js
@@ -1,10 +1,12 @@
 import React from "react";
-import "./css/Cards.css";
+import cssClasses from "./css/Cards.css";
 
 const Cards = props => {
   return (
-    <div className="Card">
-      <div className="Card-header">Hey i'm {props.name} your card</div>
+    <div className={cssClasses.Card}>
+      <div className={cssClasses.CardHeader}>
+        Hey i'm {props.name} your card
+      </div>
       <p>{props.children}</p>
       <input type="text" onChange={props.changed} value={props.name} />
     </div>
